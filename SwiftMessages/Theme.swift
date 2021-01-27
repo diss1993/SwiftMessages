c752f7ba3bcb63e8edc80c1890269f47c32a8fe8
dmitry <Buza.dima93@gmail.com>
2021-01-27 17:14:28 +0600

    Removed unused code and resources

--- SwiftMessages/Theme.swift
+++ SwiftMessages/Theme.swift
@@ -11,25 +11,13 @@ import UIKit
 /// The theme enum specifies the built-in theme options
 public enum Theme {
     case info
-    case success
-    case warning
-    case error
 }
 
 /// The Icon enum provides type-safe access to the included icons.
 public enum Icon: String {
-    
-    case error = "errorIcon"
-    case warning = "warningIcon"
-    case success = "successIcon"
+
     case info = "infoIcon"
-    case errorLight = "errorIconLight"
-    case warningLight = "warningIconLight"
-    case successLight = "successIconLight"
     case infoLight = "infoIconLight"
-    case errorSubtle = "errorIconSubtle"
-    case warningSubtle = "warningIconSubtle"
-    case successSubtle = "successIconSubtle"
     case infoSubtle = "infoIconSubtle"
     
     /// Returns the associated image.
@@ -52,15 +40,6 @@ public enum IconStyle {
         case (.info, .default): return Icon.info.image
         case (.info, .light): return Icon.infoLight.image
         case (.info, .subtle): return Icon.infoSubtle.image
-        case (.success, .default): return Icon.success.image
-        case (.success, .light): return Icon.successLight.image
-        case (.success, .subtle): return Icon.successSubtle.image
-        case (.warning, .default): return Icon.warning.image
-        case (.warning, .light): return Icon.warningLight.image
-        case (.warning, .subtle): return Icon.warningSubtle.image
-        case (.error, .default): return Icon.error.image
-        case (.error, .light): return Icon.errorLight.image
-        case (.error, .subtle): return Icon.errorSubtle.image
         default: return nil
         }
     }
