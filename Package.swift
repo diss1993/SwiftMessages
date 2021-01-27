f3ad355dd9caf9ed76ad4a6c8dbf70b7256c939f
dmitry <Buza.dima93@gmail.com>
2021-01-27 17:36:13 +0600

    Changed min version and removed unused resources

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@ import PackageDescription
 
 let package = Package (
     name: "SwiftMessages",
+    platforms: [
+        .iOS(.v10),
+    ],
     products: [
         .library(
             name: "SwiftMessages",
